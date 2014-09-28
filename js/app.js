585dbdc341d5da395287f43436b8295381947b0f
ccoenraets <ccoenraets@gmail.com>
2014-09-28 12:32:50 -0400

    initial salesforce integration

--- js/app.js
+++ js/app.js
@@ -4,69 +4,89 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'forceng', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .run(function ($ionicPlatform, $state, force) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+            // Get reference to Salesforce OAuth plugin
+            var oauthPlugin = cordova.require("com.salesforce.plugin.oauth");
 
-    .state('app', {
-      url: "/app",
-      abstract: true,
-      templateUrl: "templates/menu.html",
-      controller: 'AppCtrl'
+            // Authenticate
+            oauthPlugin.getAuthCredentials(
+                function (creds) {
+                    console.log(JSON.stringify(creds));
+                    // Initialize ForceJS
+                    force.init({accessToken: creds.accessToken, instanceURL: creds.instanceUrl, refreshToken: creds.refreshToken});
+                    $state.go('app.contactlist');
+                },
+                function (error) {
+                    console.log(error);
+                }
+            );
+
+        });
     })
 
-    .state('app.search', {
-      url: "/search",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/search.html"
-        }
-      }
-    })
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-    .state('app.browse', {
-      url: "/browse",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/browse.html"
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/menu.html"
+            })
 
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
-        }
-      }
+            .state('app.contactlist', {
+                url: "/contactlist",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/contact-list.html",
+                        controller: 'ContactListCtrl'
+                    }
+                }
+            })
+
+            .state('app.contact', {
+                url: "/contacts/:contactId",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/contact.html",
+                        controller: 'ContactCtrl'
+                    }
+                }
+            })
+
+            .state('app.accountlist', {
+                url: "/accountlist",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/account-list.html",
+                        controller: 'AccountListCtrl'
+                    }
+                }
+            })
+
+            .state('app.account', {
+                url: "/accounts/:accountId",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/account.html",
+                        controller: 'AccountCtrl'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+//        $urlRouterProvider.otherwise('/app/playlists');
     });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
 
