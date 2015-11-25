c97ccabb2455fe44dfdf5fa4651104d81c17febc
ccoenraets <ccoenraets@gmail.com>
2015-11-25 08:49:32 -0500

    updating to latest version of Ionic and forceng

--- js/app.js
+++ js/app.js
@@ -4,96 +4,118 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'forceng', 'starter.controllers'])
+angular.module('starter', ['ionic', 'forceng', 'starter.controllers', 'config'])
 
-    .run(function ($ionicPlatform, $state, force) {
-        $ionicPlatform.ready(function () {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            }
-            if (window.StatusBar) {
-                // org.apache.cordova.statusbar required
-                StatusBar.styleDefault();
-            }
+  .run(function ($ionicPlatform, $state, force, forcengOptions) {
 
-            // Authenticate using Salesforce OAuth
-            force.login().then(function() {
-                $state.go('app.contactlist');
-            });
+    $ionicPlatform.ready(function () {
 
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        window.cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+
+      // Initialize forceng
+      force.init(forcengOptions);
+
+
+      if (forcengOptions.accessToken) {
+        // If the accessToken was provided (typically when running the app from within a Visualforce page,
+        // go straight to the contact list
+        $state.go('app.contactlist');
+      } else {
+        // Otherwise (the app is probably running as a standalone web app or as a hybrid local app with the
+        // Mobile SDK, login first.)
+        force.login().then(function () {
+          $state.go('app.contactlist');
         });
-    })
-
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-
-            .state('app', {
-                url: "/app",
-                abstract: true,
-                templateUrl: "templates/menu.html",
-                controller: 'AppCtrl'
-            })
-
-            .state('app.contactlist', {
-                url: "/contactlist",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/contact-list.html",
-                        controller: 'ContactListCtrl'
-                    }
-                }
-            })
-
-            .state('app.contact', {
-                url: "/contacts/:contactId",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/contact.html",
-                        controller: 'ContactCtrl'
-                    }
-                }
-            })
-
-            .state('app.edit-contact', {
-                url: "/edit-contact/:contactId",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/edit-contact.html",
-                        controller: 'EditContactCtrl'
-                    }
-                }
-            })
-
-            .state('app.add-contact', {
-                url: "/create-contact",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/edit-contact.html",
-                        controller: 'CreateContactCtrl'
-                    }
-                }
-            })
-
-            .state('app.accountlist', {
-                url: "/accountlist",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/account-list.html",
-                        controller: 'AccountListCtrl'
-                    }
-                }
-            })
-
-            .state('app.account', {
-                url: "/accounts/:accountId",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/account.html",
-                        controller: 'AccountCtrl'
-                    }
-                }
-            });
+      }
+      $state.go('app.contactlist');
 
     });
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider, baseURL) {
+
+    // baseURL (defined in the config.js module) is only there to support running the same app as a Mobile SDK
+    // hybrid local and hybrid remote app (where the app is run from withing a Visualforce page). When running the
+    // app inside a Visualforce page, you have to account for the path of the app's static resource. To accomplish
+    // that, you create the config module from within the VF page (as opposed to importing config.js), and set
+    // baseURL to the app's static resource path.
+
+    $stateProvider
+
+      .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: baseURL + "templates/menu.html",
+        controller: 'AppCtrl'
+      })
+
+      .state('app.contactlist', {
+        url: "/contactlist",
+        views: {
+          'menuContent': {
+            templateUrl: baseURL + "templates/contact-list.html",
+            controller: 'ContactListCtrl'
+          }
+        }
+      })
+
+      .state('app.contact', {
+        url: "/contacts/:contactId",
+        views: {
+          'menuContent': {
+            templateUrl: baseURL + "templates/contact.html",
+            controller: 'ContactCtrl'
+          }
+        }
+      })
+
+      .state('app.edit-contact', {
+        url: "/edit-contact/:contactId",
+        views: {
+          'menuContent': {
+            templateUrl: baseURL + "templates/edit-contact.html",
+            controller: 'EditContactCtrl'
+          }
+        }
+      })
+
+      .state('app.add-contact', {
+        url: "/create-contact",
+        views: {
+          'menuContent': {
+            templateUrl: baseURL + "templates/edit-contact.html",
+            controller: 'CreateContactCtrl'
+          }
+        }
+      })
+
+      .state('app.accountlist', {
+        url: "/accountlist",
+        views: {
+          'menuContent': {
+            templateUrl: baseURL + "templates/account-list.html",
+            controller: 'AccountListCtrl'
+          }
+        }
+      })
+
+      .state('app.account', {
+        url: "/accounts/:accountId",
+        views: {
+          'menuContent': {
+            templateUrl: baseURL + "templates/account.html",
+            controller: 'AccountCtrl'
+          }
+        }
+      });
+
+  });
